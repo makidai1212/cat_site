7983b6d0204fa83473e9937301c83b63e8044823
makidai1212 <gachapinninaritaina@yahoo.co.jp>
2021-06-02 08:50:19 +0000

    Implement advanced login

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,10 +2,12 @@ class SessionsController < ApplicationController
   def new
   end
 
+  # sessionでダメならcookiesから探す
   def create
     user = User.find_by(email: params[:session][:email].downcase)
     if user && user.authenticate(params[:session][:password])
       log_in(user)
+      params[:session][:remember_me] == '1' ? remember(user) : forget(user)
       redirect_to user_path(user)
     else
       flash.now[:danger] = "emailかパスワードが正しくありません"
@@ -14,7 +16,7 @@ class SessionsController < ApplicationController
   end
 
   def destroy
-    log_out
+    log_out if logged_in?
     redirect_to root_url
   end
 end
