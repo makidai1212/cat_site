cf3090532fa010751d01333a4dedef54e2c43330
makidai1212 <gachapinninaritaina@yahoo.co.jp>
2021-06-08 09:38:19 +0000

    Add password reset

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'password_resets/new'
+  get 'password_resets/edit'
   get '/signup', to: 'users#new'
   get '/home', to: 'static_pages#home'
   get '/about', to: 'static_pages#about'
@@ -9,4 +11,5 @@ Rails.application.routes.draw do
   post '/login', to: 'sessions#create'
   delete '/logout', to: 'sessions#destroy'
   resources :account_activations, only: :edit
+  resources :password_resets, only: [:new, :create, :edit, :update]
 end
