7d22623ed6a2ec59e4bb9603ceb3d1abf9daa65c
makidai1212 <gachapinninaritaina@yahoo.co.jp>
2021-06-24 05:31:11 +0000

    layout change in master branch

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -16,11 +16,13 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
+      flash[:success] = "登録に成功しました！"
       # @user.send_activation_email
       # flash[:info] = "メール送信しました。メールからアカウント有効化してください"
       # redirect_to root_url
       
       # アカウント有効化機能を復活させる時はこちらをコメントアウトすること
+      log_in @user
       redirect_to @user
     else
       render "new"
