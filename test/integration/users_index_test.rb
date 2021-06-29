452353aa3ba49851ee71ad431090a81945418436
makidai1212 <gachapinninaritaina@yahoo.co.jp>
2021-06-29 02:38:01 +0000

    OK commit

--- test/integration/users_index_test.rb
+++ test/integration/users_index_test.rb
@@ -12,10 +12,10 @@ class UsersIndexTest < ActionDispatch::IntegrationTest
     get users_path
     assert_template 'users/index'
     assert_select 'div.pagination'
-     User.paginate(page: 1).each do |user|
+    User.limit(15).page(1).each do |user|
       assert_select 'a[href=?]', user_path(user), text: user.name
     end
-    first_page_of_users = User.paginate(page: 1)
+    first_page_of_users = User.limit(15).page(1)
     first_page_of_users.each do |user|
       assert_select "a[href=?]", user_path(user), text: user.name
      unless user == @admin
