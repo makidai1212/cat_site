01f529bf204ec86f19fd2fab58a675ecf8f0cd5a
makidai1212 <gachapinninaritaina@yahoo.co.jp>
2021-07-05 10:00:31 +0000

    title sukoshi dekakusita

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -4,15 +4,16 @@ class StaticPagesController < ApplicationController
     if logged_in?
       @micropost = current_user.microposts.build
       @feed_items = current_user.feed.paginate(page: params[:page])
+      
+      # 全部表示させるための設定
+      @article = Micropost.order(created_at: :desc).limit(15).page(params[:page])
+      @paginatable_array = Kaminari.paginate_array([], total_count: 150).page(params[:page])
+      # ランキング表示のための設定
+      @all_ranks = Micropost.find(Like.group(:micropost_id).order('count(micropost_id) desc').limit(5).pluck(:micropost_id))
+      
+      ＠randams = Micropost.order("RAND()").limit(3)
     end
 
-    # 全部表示させるための設定
-    @article = Micropost.order(created_at: :desc).limit(15).page(params[:page])
-    @paginatable_array = Kaminari.paginate_array([], total_count: 150).page(params[:page])
-    # ランキング表示のための設定
-    @all_ranks = Micropost.find(Like.group(:micropost_id).order('count(micropost_id) desc').limit(5).pluck(:micropost_id))
-    
-    ＠randams = Micropost.order("RAND()").limit(3)
   end
 
   def about
