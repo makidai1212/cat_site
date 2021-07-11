4ff90d2593f7237991e7197a9399d3f147238ab2
makidai1212 <gachapinninaritaina@yahoo.co.jp>
2021-07-11 11:28:21 +0000

    yokuwakaran

--- test/integration/likes_count_test.rb
+++ test/integration/likes_count_test.rb
@@ -2,4 +2,17 @@ require 'test_helper'
 
 class LikesCountTest < ActionDispatch::IntegrationTest
 
+  def setup
+    @user = users(:michael)
+    @other = users(:archer)
+    @micropost = microposts(:orange)
+    log_in_as(@user)
+  end
+
+  test "お気に入りが機能する" do
+    assert_difference '@micropost.likes.count',1 do
+      post likes_path, params: { user_id: @other.id, micropost_id: @micropost.id}
+    end
+  end
+
 end
